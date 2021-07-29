e2c5413552c729f963304a595522189a3ac95aae
Rafael Martínez Costas <it-rafamartinez@outlook.es>
2021-07-29 14:28:24 +0200

    comments in code, extension to linux executables="out", others

---
+++ book/chapter_1/section_1.01/example_1.1_2/src/helloworld_2.c
@@ -0,0 +1,16 @@
+/** @file Example 2 from page 8
+ * @author 
+ */
+
+// Library needed to call printf
+#include <stdio.h>
+
+/** Send "hello, world\n" to output. Versión 2
+ * 
+ * @return (implicit int) state of program
+ */
+main(){
+	printf("hello, ");
+	printf("world");
+	printf("\n");
+}
